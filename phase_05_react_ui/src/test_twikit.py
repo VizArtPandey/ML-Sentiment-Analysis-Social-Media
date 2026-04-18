147fe707e16181fc6f33b96c3ba1028b3ea4b636
VizArt Pandey <VizArtPandey@users.noreply.github.com>
2026-04-19 02:20:09 +0400

    chore: clean up unnecessary temporary test and patch files

--- phase_05_react_ui/src/test_twikit.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from twikit import Client
-
-async def main():
-    client = Client('en-US')
-    tweets = await client.search_tweet('python', 'Latest')
-    print(tweets)
-
-asyncio.run(main())
